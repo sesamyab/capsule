42b30daceb4a73c2dc0c25222f3090074e9cc177
Markus Ahlstrand <markus@ahlstrand.es>
2026-03-02 16:50:36 +0100

    Rename nav

--- apps/demo/src/components/Navigation.tsx
+++ apps/demo/src/components/Navigation.tsx
@@ -11,7 +11,7 @@ export function Navigation() {
           <Link href="/spec">Specification</Link>
           <Link href="/glossary">Glossary</Link>
           <Link href="/client">Client</Link>
-          <Link href="/servers">Servers</Link>
+          <Link href="/server">Server</Link>
           <Link href="/cms-test">CMS API</Link>
           <Link href="/demo">Demo</Link>
         </div>
